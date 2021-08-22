0aa9172651ff6cdabcdd0c764afcef43adda0aae
hectorgabucio <hectorcompangabucio@gmail.com>
2021-08-22 18:09:00 +0200

    husky and code fixes

--- src/commands.ts
+++ src/commands.ts
@@ -1,17 +1,16 @@
-import { spawn } from "child_process";
-import { TextChannel, VoiceState } from "discord.js";
-import fs from "fs";
-import { promisify } from "util";
+import { spawn } from 'child_process';
+import { TextChannel, VoiceState } from 'discord.js';
+import fs from 'fs';
+import { promisify } from 'util';
 
 const createNewChunk = (SessionID: string) => {
-  const pathToFile =
-    __dirname + `/../recordings/${SessionID}/${Date.now()}.pcm`;
+  const pathToFile = __dirname + `/../recordings/${SessionID}/${Date.now()}.pcm`;
   return fs.createWriteStream(pathToFile);
 };
 
 const mkdir = promisify(fs.mkdir);
-let voiceSessionMap: Record<string | number, any> = {};
-let activeGuildRecorders: Record<string, boolean> = {};
+const voiceSessionMap: Record<string | number, any> = {};
+const activeGuildRecorders: Record<string, boolean> = {};
 export const enter = async function (
   guildId: string | number,
   authorName: string,
@@ -20,38 +19,28 @@ export const enter = async function (
     join: () => Promise<any>;
     id: string | number;
     guild: { name: any };
-  }
+  },
 ) {
-  if (typeof activeGuildRecorders[guildId] === "undefined") {
+  if (typeof activeGuildRecorders[guildId] === 'undefined') {
     const start = process.hrtime();
 
-    console.log(
-      "enabling lock for guild id",
-      guildId,
-      "last",
-      process.hrtime(start)
-    );
-    let currentGuildId = guildId;
+    console.log('enabling lock for guild id', guildId, 'last', process.hrtime(start));
+    const currentGuildId = guildId;
     activeGuildRecorders[currentGuildId] = true;
-    let voiceSid = authorName + "-" + new Date().toISOString();
+    const voiceSid = authorName + '-' + new Date().toISOString();
 
     await mkdir(__dirname + `/../recordings/${voiceSid}/`, { recursive: true });
 
-    console.log(
-      `Sliding into ${voiceChannel.name} ..., last`,
-      process.hrtime(start)
-    );
+    console.log(`Sliding into ${voiceChannel.name} ..., last`, process.hrtime(start));
 
     voiceChannel
       .join()
       .then((conn) => {
-        console.log("inside conn, last", process.hrtime(start));
-        let deltaStart = Date.now();
-        const dispatcher = conn.play(
-          __dirname + "/../sounds/taterubot-start-recording.mp3"
-        );
-        console.log("after dispatcher, last", process.hrtime(start));
-        dispatcher.on("finish", () => {
+        console.log('inside conn, last', process.hrtime(start));
+        const deltaStart = Date.now();
+        const dispatcher = conn.play(__dirname + '/../sounds/taterubot-start-recording.mp3');
+        console.log('after dispatcher, last', process.hrtime(start));
+        dispatcher.on('finish', () => {
           console.log(`Joined ${voiceChannel.name}!\n\nREADY TO RECORD\n`);
         });
         voiceSessionMap[voiceChannel.id] = {
@@ -64,9 +53,9 @@ export const enter = async function (
           recordStart: deltaStart,
         };
         const receiver = conn.receiver;
-        conn.on("speaking", (user: { username: any }, speaking: any) => {
+        conn.on('speaking', (user: { username: any }, speaking: any) => {
           if (speaking) {
-            let delta = Date.now() - deltaStart;
+            const delta = Date.now() - deltaStart;
             /*
                         e: EventType
                             "s": Speak start
@@ -75,18 +64,18 @@ export const enter = async function (
                         d: Delta from 
                     */
             voiceSessionMap[voiceChannel.id].activityLog.push({
-              e: "s",
+              e: 's',
               s: user.username,
               d: delta,
             });
             console.log(`${user.username} started speaking`);
-            const audioStream = receiver.createStream(user, { mode: "pcm" });
+            const audioStream = receiver.createStream(user, { mode: 'pcm' });
             audioStream.pipe(createNewChunk(voiceSid));
-            audioStream.on("end", () => {
-              let deltaEnd = Date.now() - deltaStart;
+            audioStream.on('end', () => {
+              const deltaEnd = Date.now() - deltaStart;
               console.log(`${user.username} stopped speaking`);
               voiceSessionMap[voiceChannel.id].activityLog.push({
-                e: "e",
+                e: 'e',
                 s: user.username,
                 d: deltaEnd,
               });
@@ -96,10 +85,10 @@ export const enter = async function (
       })
       .catch((err) => {
         console.log(err);
-        console.warn("Failure connecting to guild");
+        console.warn('Failure connecting to guild');
       });
   } else {
-    console.log("An active recording session exists in the current guild.");
+    console.log('An active recording session exists in the current guild.');
   }
 };
 export const exit = function (voice: VoiceState, channel: TextChannel) {
@@ -107,56 +96,42 @@ export const exit = function (voice: VoiceState, channel: TextChannel) {
 
   const { channel: voiceChannel, connection: conn } = voice;
   if (!voiceChannel || !conn) {
-    throw new Error("wtf no voice channel or conn");
+    throw new Error('wtf no voice channel or conn');
   }
   const resolveSessionId = voiceSessionMap[voiceChannel.id].voiceSid;
-  const dispatcher = conn.play(__dirname + "/../sounds/badumtss.mp3", {
+  const dispatcher = conn.play(__dirname + '/../sounds/badumtss.mp3', {
     volume: 0.45,
   });
-  dispatcher.on("finish", () => {
-    let data = JSON.stringify(voiceSessionMap[voiceChannel.id]);
-    fs.writeFile(
-      __dirname + `/../recordings/${resolveSessionId}.json`,
-      data,
-      "utf8",
-      (err) => {
-        if (err) {
-          return console.log(err);
-        }
-
-        console.log(
-          "written stats json",
-          __dirname + `/../recordings/${resolveSessionId}.json`
-        );
-        const nodeArgs = [__dirname + "/../bin/merge.js", resolveSessionId];
-        const transcoderChild = spawn("node", nodeArgs);
-        transcoderChild.stdout.setEncoding("utf8");
-        transcoderChild.stdout.on("data", function (data) {
-          //Here is where the output goes
-
-          console.log("transcoder stdout " + data);
-
-          data = data.toString();
-        });
-        transcoderChild.on("exit", function (code, signal) {
-          console.log(
-            "Transcoder process exited with " +
-              `code ${code} and signal ${signal}`
-          );
-
-          if (code === 0) {
-            const path = `./recordings/${resolveSessionId}/${resolveSessionId}.mp3`;
-            channel.send({
-              files: [path],
-            });
-          }
-        });
+  dispatcher.on('finish', () => {
+    const data = JSON.stringify(voiceSessionMap[voiceChannel.id]);
+    fs.writeFile(__dirname + `/../recordings/${resolveSessionId}.json`, data, 'utf8', (err) => {
+      if (err) {
+        return console.log(err);
       }
-    );
-    console.log(
-      "Destroying guild lock for",
-      voiceSessionMap[voiceChannel.id].guildId
-    );
+
+      console.log('written stats json', __dirname + `/../recordings/${resolveSessionId}.json`);
+      const nodeArgs = [__dirname + '/../bin/merge.js', resolveSessionId];
+      const transcoderChild = spawn('node', nodeArgs);
+      transcoderChild.stdout.setEncoding('utf8');
+      transcoderChild.stdout.on('data', function (data) {
+        //Here is where the output goes
+
+        console.log('transcoder stdout ' + data);
+
+        data = data.toString();
+      });
+      transcoderChild.on('exit', function (code, signal) {
+        console.log('Transcoder process exited with ' + `code ${code} and signal ${signal}`);
+
+        if (code === 0) {
+          const path = `./recordings/${resolveSessionId}/${resolveSessionId}.mp3`;
+          channel.send({
+            files: [path],
+          });
+        }
+      });
+    });
+    console.log('Destroying guild lock for', voiceSessionMap[voiceChannel.id].guildId);
     delete activeGuildRecorders[voiceSessionMap[voiceChannel.id].guildId];
     delete voiceSessionMap[voiceChannel.id];
     voiceChannel.leave();
--- src/index.ts
+++ src/index.ts
@@ -4,67 +4,59 @@ const client = new Discord.Client();
 const config = require('../config.json');
 import { enter, exit } from './commands';
 
+const POOL = 2;
+const CHANNEL_PREFIX = 'TATERU-';
 
-const POOL = 2
-const CHANNEL_PREFIX = 'TATERU-'
+const MAP: Record<string, Discord.GuildChannel[]> = {};
 
-
-
-const MAP:Record<string, any> = {}
-
-const USER_RECORDING:Record<string, string|undefined> = {}
+const USER_RECORDING: Record<string, string | undefined> = {};
 
 client.login(config.BOT_TOKEN);
 
 client.on('ready', async () => {
-    console.log(`\nONLINE\n`);
-    for (const guild of client.guilds.cache) {
-        const tateChannels = guild[1].channels.cache.array().filter(x => x.name.startsWith(CHANNEL_PREFIX) && x.type === 'voice')
-        const newChannels = []
-        if (tateChannels.length < POOL) {
-            for (let i = tateChannels.length; i < POOL; i++) {
-                const chan = await guild[1].channels.create(CHANNEL_PREFIX + i, {userLimit: 2, type: 'voice'})
-                newChannels.push(chan)
-            }
-
-        }
-        const finalChannels = tateChannels.concat(newChannels)
-        MAP[guild[0]] = finalChannels
-
+  console.log(`\nONLINE\n`);
+  for (const guild of client.guilds.cache) {
+    const tateChannels = guild[1].channels.cache
+      .array()
+      .filter((x) => x.name.startsWith(CHANNEL_PREFIX) && x.type === 'voice');
+    const newChannels = [];
+    if (tateChannels.length < POOL) {
+      for (let i = tateChannels.length; i < POOL; i++) {
+        const chan = await guild[1].channels.create(CHANNEL_PREFIX + i, { userLimit: 2, type: 'voice' });
+        newChannels.push(chan);
+      }
     }
+    const finalChannels = tateChannels.concat(newChannels);
+    MAP[guild[0]] = finalChannels;
+  }
 });
 
 client.on('voiceStateUpdate', (oldMember, newMember) => {
+  if (oldMember?.member?.user?.bot || newMember?.member?.user?.bot) {
+    console.log('fucking bot...');
+    return;
+  }
 
-    if (oldMember?.member?.user?.bot || newMember?.member?.user?.bot) {
-        console.log('fucking bot...')
-        return
+  const guildNew = newMember.guild.id;
+  const guildObj = newMember.guild;
+
+  if (newMember.channel === null) {
+    if (oldMember?.channel?.name.startsWith(CHANNEL_PREFIX)) {
+      if (USER_RECORDING[guildNew] === oldMember?.member?.id) {
+        console.log('stop recording biatch');
+        const channel = guildObj.channels.cache.find((x) => x.type === 'text');
+        guildObj.voice && channel && exit(guildObj.voice, channel as TextChannel);
+
+        USER_RECORDING[guildNew] = undefined;
+      }
+      console.log(USER_RECORDING[guildNew], oldMember?.member?.user.id);
     }
+    console.log('leaving wtf');
+    return;
+  }
 
-    const guildNew = newMember.guild.id
-    const guildObj = newMember.guild
-    const channNew = newMember.channelID
-
-
-    if (newMember.channel === null) {
-        if (oldMember?.channel?.name.startsWith(CHANNEL_PREFIX)) {
-            if (USER_RECORDING[guildNew] === oldMember?.member?.id) {
-                console.log('stop recording biatch')
-                const channel =  guildObj.channels.cache.find(x => x.type === 'text')
-                guildObj.voice && channel && exit(guildObj.voice,(channel as TextChannel))
-
-
-                USER_RECORDING[guildNew] = undefined
-            }
-            console.log(USER_RECORDING[guildNew],oldMember?.member?.user.id)
-        }
-        console.log('leaving wtf')
-        return
-    }
-
-    if (newMember.channel.name.startsWith(CHANNEL_PREFIX)) {
-        USER_RECORDING[guildNew] = newMember?.member?.user.id
-        enter(guildNew, newMember?.member?.user?.username ?? 'pepoclown',newMember.channel)
-    }
-
- });
+  if (newMember.channel.name.startsWith(CHANNEL_PREFIX)) {
+    USER_RECORDING[guildNew] = newMember?.member?.user.id;
+    enter(guildNew, newMember?.member?.user?.username ?? 'pepoclown', newMember.channel);
+  }
+});
